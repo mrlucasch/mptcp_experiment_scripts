e68ab915d37d53fb743468109dd454aa77ccdac1
Lucas <lucasch@cs>
2017-12-04 13:05:25 -0500

    updated bittorrent scripts

--- bittorrent/create_tracker.sh
+++ bittorrent/create_tracker.sh
@@ -2,7 +2,8 @@
 
 
 name=$1
+tracker=$2
 
 
-
-ctorrent -t -u "http://10.16.3.92:6969/announce" -s ${name}.torrent $name
+#ctorrent -t -u "http://10.16.3.92:6969/announce" -s ${name}.torrent $name
+transmission-create -o ${name}.torrent -c "${name} Made using transmission create" -t http://${tracker}:6969/announce ${name}
--- bittorrent/download.sh
+++ bittorrent/download.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 name=$1
-ctorrent -D 1184082.031  $name
+#ctorrent -D 1184082.031  $name
+time ctorrent ${name} -e 0
